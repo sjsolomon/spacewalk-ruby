85cd85684d3ec684c88672e922c63cb32e4d2cf2
Shane <shane@Shanes-MacBook-Pro.local>
2018-02-26 21:19:27 -0600

    Finished up the connection and working on simple api calls

--- lib/spacewalk/client.rb
+++ lib/spacewalk/client.rb
@@ -1,6 +1,8 @@
 module Spacewalk
   # Document here TODO: Actually document this
   class Client
+    include Spacewalk::API
+
     def initialize(**opts)
       @config = Spacewalk::Config.new(opts)
     end
--- lib/spacewalk/config.rb
+++ lib/spacewalk/config.rb
@@ -1,6 +1,6 @@
 module Spacewalk
   CONFIG_OPTIONS = {
-    host: '',
+    host: 'localhost',
     user: '',
     password: '',
     http_verify: 'true'
@@ -12,6 +12,14 @@ module Spacewalk
       CONFIG_OPTIONS.each do |name, value|
         instance_variable_set "@#{name}", options.fetch(name, value)
       end
+      @auth_key = open_connection
+    end
+
+    private
+    def open_connection
+      conn_opts = XMLRPC::Client.new2("https://#{@host}/rpc/api")
+      conn_opts.http.verify_mode = OpenSSL::SSL::VERIFY_NONE unless @http_verify
+      conn_opts.call('auth.login', @user, @password)
     end
   end
 end
